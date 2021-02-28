c0a831c55f9003c99509359d8192805d48f44192
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-02-28 15:04:30 -0300

    minor-fixes

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import map from '../assets/imgs/mapa.png';
+import logos from '../assets/imgs/logos.png';
 
 export default function StartPage(props) {
   useEffect(() => {
@@ -15,6 +16,7 @@ export default function StartPage(props) {
         <span>Trip</span>
       </div>
       <img className="map-image" src={map} alt="Mapa" />
+      <span className="logos"><img src={logos} alt={logos} /></span>
     </>
   )
 }
